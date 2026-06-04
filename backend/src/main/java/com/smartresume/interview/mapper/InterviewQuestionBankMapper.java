90f9143a7e96cd06a33d3b44191126c350a1afe7
OrtonY <85286889+OrtonY@users.noreply.github.com>
2026-06-04 21:32:01 +0800

    Add interview question bank (#56)
    * feat: tighten question bank responsive scrolling
    
    * feat: add interview question bank support
    
    * chore(task): archive 06-03-interview-question-bank
    
    * chore: record journal

---
+++ backend/src/main/java/com/smartresume/interview/mapper/InterviewQuestionBankMapper.java
@@ -0,0 +1,7 @@
+package com.smartresume.interview.mapper;
+
+import com.mybatisflex.core.BaseMapper;
+import com.smartresume.interview.domain.InterviewQuestionBankEntity;
+
+public interface InterviewQuestionBankMapper extends BaseMapper<InterviewQuestionBankEntity> {
+}
